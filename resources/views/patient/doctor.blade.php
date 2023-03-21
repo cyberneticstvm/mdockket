f09d6fb4930c82b625ed8711ca1ed4e53a82d812
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-21 21:24:49 +0530

    small updates

--- resources/views/patient/doctor.blade.php
+++ resources/views/patient/doctor.blade.php
@@ -47,7 +47,7 @@
                     </div>
                     <div class="mb-3 input-group input-mini">
                         <div id="datepicker" class="input-group date" data-date-format="dd-M-yyyy">
-                            <input class="form-control" type="text" value="{{ ($input && $input[5]) ? $input[5] : '' }}" name="date" />
+                            <input class="form-control" type="text" value="{{ ($input && $input[5]) ? $input[5] : $input[5] }}" name="date" />
                             <span class="input-group-addon">
                                 <i class="glyphicon glyphicon-calendar"></i>
                             </span>
