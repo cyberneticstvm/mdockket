7eaa00aae04e34e07443e3e91927402cb8a0526c
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-21 21:14:09 +0530

    small updates

--- resources/views/doctor/appointments.blade.php
+++ resources/views/doctor/appointments.blade.php
@@ -24,7 +24,7 @@
                     @if($c == $settings->slots) @break; @endif
                     <div class="col-md-3 slot1 {{ (in_array(date('h:i A', $from), $atimes)) ? 'bg-success text-white no-app' : '' }}">
                         {{ date('h:i A', $from) }}<br>
-                        @if(in_array(date('h:i A', $from), $atimes) || (date('h:i A', $from) >= date('h:i A', $bstime) && date('h:i A', $from) <= date('h:i A', $betime)))
+                        @if(in_array(date('h:i A', $from), $atimes) || ($from >= $bstime && $from <= $betime))
                             @php $index = array_search(date('h:i A', $from), $atimes); @endphp
                             Patient Name: {!! ($index != '') ? $apps[$index]->patient_name : "<span class='fw-bold text-danger'>Break Time</span>" !!}<br>
                             Contact No: {{ ($index != '') ? $apps[$index]->mobile : '' }}
