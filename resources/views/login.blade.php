135e658e50bc88c2923fbb447069251d2ed136b8
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-22 21:01:36 +0530

    small updates

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,8 +8,7 @@
             <div class="circle-1"></div>
             <div class="container inner-wrapper">
                 <div class="welcome-logo">
-                    <img src="{{ public_path().'/assets/dockket/dockket-icon.png' }}" class="img-fluid" width="25%">
-                    <div class="get-started">Your health care token<div>
+                    <img src="{{ public_path().'/assets/dockket/dockket-icon.png' }}" class="img-fluid" style="width:25%">
                 </div>
             </div>
         </div>
