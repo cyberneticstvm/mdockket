bf91ce31d5e41135c75c805b4b6d1b0a4141931f
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-22 21:15:27 +0530

    small updates

--- resources/views/patient/profile.blade.php
+++ resources/views/patient/profile.blade.php
@@ -6,12 +6,12 @@
         <div class="container">
             <div class="header-content">
                 <div class="left-content">
-                    <a href="javascript:void(0);" class="back-btn">
+                    <!--<a href="javascript:void(0);" class="back-btn">
                         <svg width="18" height="18" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.03033 0.46967C9.2966 0.735936 9.3208 1.1526 9.10295 1.44621L9.03033 1.53033L2.561 8L9.03033 14.4697C9.2966 14.7359 9.3208 15.1526 9.10295 15.4462L9.03033 15.5303C8.76406 15.7966 8.3474 15.8208 8.05379 15.6029L7.96967 15.5303L0.96967 8.53033C0.703403 8.26406 0.679197 7.8474 0.897052 7.55379L0.96967 7.46967L7.96967 0.46967C8.26256 0.176777 8.73744 0.176777 9.03033 0.46967Z" fill="#a19fa8"/>
                         </svg>
                     </a>
-                    <h5 class="mb-0 ms-2">Profile</h5>
+                    <h5 class="mb-0 ms-2">Profile</h5>-->
                 </div>
                 <div class="mid-content">
                 </div>
@@ -38,7 +38,7 @@
                 </div>
             @endif
             <div class="media media-100">
-                <img src="{{ public_path().'/assets/images/message/pic6.jpg' }}" alt="/">
+                <img src="{{ public_path().'/assets/dockket/Happy-Patient-300x300.png' }}" alt="/">
                 <svg class="progress-style" height="100" width="100">
                     <circle id="round-1" cx="60" cy="60" r="50" stroke="#E8EFF3" stroke-width="7" fill="none" />
                     <circle id="round-2" cx="60" cy="60" r="50" stroke="#C3AC58" stroke-width="7" fill="none" />
